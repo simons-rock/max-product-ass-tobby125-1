73c7c9abee0a049dd92a84060dd15d6fa4201ff8
Daniel Rose-Levine <droselevine18@simons-rock.edu>
2019-01-29 20:25:55 -0500

    turn in

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,17 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	int max1 = 0;
+	int max2 = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] >= max1) {
+			max2 = max1;
+			max1 = arr[i];
+		}
+		else if (arr[i] > max2) {
+			max2 = arr[i];
+		}	
+	}
+	return max1 * max2;
 }
 
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,13 @@
 #include "max_product.h"
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+	int max = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (i != j && arr[i] * arr[j] > max) {
+				max = arr[i] * arr[j];
+			}
+		}
+	}
+	return max;
 }
